226ab8097fea9f4909b24c41181f16f88330981b
Namyunsuk <84739562+Namyunsuk@users.noreply.github.com>
2021-07-01 07:03:50 +0900

    Create delete.php

---
+++ delete.php
@@ -0,0 +1,20 @@
+
+<?php
+    $connect = mysqli_connect("localhost", "namyunsuk", "1234", "blog") or die ("connect fail");
+    $number = $_GET[number];
+    $title = $_GET[title];
+    $content = $_GET[content];
+    $date = date('Y-m-d H:i:s');
+    $query = "delete from board where number=$number";
+    $result = $connect->query($query);
+    if($result) {
+?>
+        <script>
+            alert("삭제되었습니다.");
+            location.replace("./index.php");
+        </script>
+<?php    }
+    else {
+        echo "fail";
+    }
+?>
